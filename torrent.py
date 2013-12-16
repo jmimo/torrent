7506f7ae5f2b605e4bfe85b73a3027d7f3d4d892
Michael Mimo Moratti <mimo@mimo.ch>
2013-12-16 18:41:55 +0100

    tested pytvdbapi

--- torrent.py
+++ torrent.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from pytbdbapi import api
+from pytvdbapi import api
 
 app = Flask(__name__)
 
@@ -17,7 +17,9 @@ def tracker():
     result = db.search('Dexter', 'en')
     show = result[0]
     show.load_banners()
-    return render_template('tracker.html', navloc='tracker')
+    amount = len(show.banner_objects)
+    bannerurl = show.banner_objects[0].banner_url
+    return render_template('tracker.html', navloc='tracker', bannerpath=bannerurl)
 
 if __name__ == '__main__':
     app.run()

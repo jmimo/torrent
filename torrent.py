696158704680cba0a4fea82c2f245b9d6f5160c8
Michael Mimo Moratti <mimo@mimo.ch>
2013-12-28 07:26:06 +0100

    in progress

--- torrent.py
+++ torrent.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from pytvdbapi import api
+import feedparser
 
 app = Flask(__name__)
+tvdb = api.TVDB('BECF7AA2B2C2B4F4',actors=False,banners=True)
 
 app.config.update(dict(
     DEBUG=True
@@ -15,25 +17,26 @@ def welcome():
 def tracker():
 
     # Feed url: feed://ezrss.it/search/index.php?simple&show_name=Big+Bang+Theory&mode=rss
+    return render_template('tracker.html', navloc='tracker')
 
-
-    # tvdb information
-    db = api.TVDB('BECF7AA2B2C2B4F4',actors=False,banners=True)
-    result = db.search('Dexter', 'en')
-    show = result[0]
-    show.load_banners()
-
-    seasons = [b for b in show.banner_objects if b.BannerType == "season" and b.BannerType2 == "seasonwide"]
-
-    return render_template('tracker.html', navloc='tracker', banners=seasons)
-
-@app.route('/search')
+@app.route('/search',methods=['POST'])
 def search():
-    return render_template('search.html', navloc='tracker')
+    searchValue = request.form['search.value']
+    # TODO: input validation
+    result = tvdb.search(searchValue, 'en')
+    if len(result) == 1:
+        return redirect(url_for('showdetails', showid=result[0].id))
+    else:
+        return render_template('search.html', navloc='home', shows=result)
 
-@app.route('/result')
-def result():
+@app.route('/showdetails/<showid>', methods=['GET'])
+def showdetails(showid):
+    tvdbshow = tvdb.get_series(showid,'en')
+    tvdbshow.update()
 
-    return render_template('search.html', navloc='tracker', data=True)
+    feed = feedparser.parse('http://www.torrentreactor.net/rss.php?search=' + tvdbshow.SeriesName)
+
+    return render_template('showdetails.html', navloc='home', show=tvdbshow, feed=feed)
+
 if __name__ == '__main__':
     app.run()
